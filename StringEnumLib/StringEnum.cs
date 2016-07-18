3c140bf89fdc7204439a617cf112ca57b34fa2d3
UHSL-Marcus <m.lee23@herts.ac.uk>
2016-07-18 13:00:05 +0100

    both UWP and .net framework

---
+++ StringEnumLib/StringEnum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+namespace StringEnum
+{
+    public class StringValueAttribute : Attribute
+    {
+
+        private string _value;
+
+        public StringValueAttribute(string value)
+        {
+            _value = value;
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }
+
+    }
+    public static class StringEnum
+    {
+        public static string GetStringValue(this Enum value)
+        {
+            string output = null;
+            Type type = value.GetType();
+
+            FieldInfo fi = type.GetField(value.ToString());
+            StringValueAttribute[] attrs =
+                fi.GetCustomAttributes(typeof(StringValueAttribute),
+                                        false) as StringValueAttribute[];
+            if (attrs.Length > 0)
+                output = attrs[0].Value;
+
+            return output;
+        }
+    }
+}
